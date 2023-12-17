7f27f17d95d871b40411cb0cb1c55536cff8ffba
jerrydev01 <jerrydev01@gmail.com>
2023-12-18 00:22:10 +0100

    m

--- src/Component/SharedLayout/MobileNav.tsx
+++ src/Component/SharedLayout/MobileNav.tsx
@@ -1,3 +1,5 @@
+import ButtonComponent from "../SharedUI/Button";
+
 interface IProps {
   naveMenu;
   setShow: React.Dispatch<React.SetStateAction<boolean>>;
@@ -6,13 +8,13 @@ interface IProps {
 const MobileNav = ({ naveMenu, setShow, show }: IProps) => {
   return (
     <div className="relative top-20">
-      <div className="  right-0 bg-white py-10 px-20 w-[30vh] sm:w-[40vh] fixed h-screen z-40 ">
-        <ul className="flex-col gap-6  ">
+      <div className="  right-0 bg-white py-10  w-full fixed h-screen z-40 overflow-clip">
+        <ul className="flex-col gap-6 justify-center items-center w-full  ">
           {naveMenu.map((item) => (
             <li
               onClick={() => setShow(!show)}
               key={item.id}
-              className="text-[24px]  font-medium hover:opacity-90 py-5"
+              className="text-[24px]  font-medium hover:opacity-90 py-5 text-center"
             >
               <a href={item.path} className="text-primary font-semibold">
                 {item.name}
@@ -20,6 +22,16 @@ const MobileNav = ({ naveMenu, setShow, show }: IProps) => {
             </li>
           ))}
         </ul>
+        <div className="flex w-full justify-center ">
+          <ButtonComponent
+            handleSubmit={() => {
+              setShow(!show);
+            }}
+            className="hover:bg-primary hover:text-white rounded-full border-2 border-primary text-primary  "
+          >
+            Log In
+          </ButtonComponent>
+        </div>
       </div>
       <div
         onClick={() => setShow(!show)}
